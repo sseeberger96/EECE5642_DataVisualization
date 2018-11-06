5fb445effb4c9c960bdbb27e66736d6f36a3f9f6
tym0027 <tarupprecht@gmail.com>
2018-11-06 15:20:51 -0500

    added kmeans into 20news

--- 20news.py
+++ 20news.py
@@ -2,12 +2,19 @@
 import string
 import numpy as np
 
+from scipy.cluster.hierarchy import ward, dendrogram
+
 # sklearn
 from sklearn.datasets import fetch_20newsgroups 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.manifold import MDS
 
 # NLTK
 from nltk import word_tokenize, sent_tokenize, regexp_tokenize
@@ -35,7 +42,7 @@ from sklearn.decomposition import PCA
 import pyLDAvis
 import pyLDAvis.gensim
 import matplotlib
-matplotlib.use("TkAgg")
+# matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 # %matplotlib inline
 
@@ -46,6 +53,21 @@ warnings.filterwarnings("ignore",category=DeprecationWarning)
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
 
+import random
+import pandas as pd
+
+import os  # for os.path.basename
+
+# import matplotlib.pyplot as plt
+# import matplotlib as mpl
+
+# PLotly is an alterative to matlibplot.
+# conda install plotly
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+py.plotly.tools.set_credentials_file(username='tym0027', api_key='3z8kCvgxHDE7IS5gVblD')
 
 
 contractions = ["n't", "'ll", "'re", "'ve", "'s", "'m", "'d", "\'s", "\'t", "\'ax"]
@@ -315,8 +337,118 @@ def makeLDA(data):
 	vis = pyLDAvis.gensim.prepare(lda, corp, docVocabDict)
 	pyLDAvis.save_html(vis, 'LDA_Visualization.html')
 
+def createDocVocab(data):
+	things = []
+	sentences = []
+	for doc in data:
+		things.append(doc.split(". "))
+	# print(things)
+
+	idx = 0;	
+	for item in things:
+		for doc in things[idx]:
+			arr = []			
+			word_arr = doc.split(" ")
+			for word in word_arr:
+				if tryWord(word):
+					arr.append(word)
+				else:
+					continue
+			sentences.append(" ".join(arr))
+		idx = idx + 1;
+	
+	return sentences
+
+def kmeans(docs):
+	cats = ["comp.windows.x", "comp.os.ms-windows.misc", "talk.politics.misc", "comp.sys.ibm.pc.hardware","talk.religion.misc","rec.autos","sci.space","talk.politics.guns","alt.atheism","misc.forsale","comp.graphics","sci.electronics","sci.crypt","soc.religion.christian","rec.sport.hockey","sci.med","rec.motorcycles","comp.sys.mac.hardware","talk.politics.mideast","rec.sport.baseball"];
+	subcats = ["comp.windows.x", "sci.med", "rec.sport.hockey", "soc.religion.christian"]
+	cats = ["comp.windows.x", "comp.os.ms-windows.misc", "comp.sys.ibm.pc.hardware","comp.graphics", "rec.autos","sci.space", "sci.med","talk.politics.misc", "talk.religion.misc", "alt.atheism", "soc.religion.christian", "rec.sport.hockey"]
+
+	twentyNewsTrain = fetch_20newsgroups(subset='train', categories=cats, shuffle=True, random_state=42, remove=('headers'))
+
+	# documents = createDocVocab(twentyNewsTrain.data[0:100])
+	documents = docs;
+
+	print(len(documents))
+
+	vectorizer = TfidfVectorizer(stop_words='english')
+	X = vectorizer.fit_transform(documents)
+
+	true_k = 12
+	model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=10)
+	model.fit(X)
+
+	print("Top terms per cluster:")
+	order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+	terms = vectorizer.get_feature_names()
+	cluster_names = {};
+	cluster_colors = {};
+	colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080']
+	
+	for i in range(true_k):
+		print("\nCluster " + str(i) + ":");
+		t = str(i) + ") ";
+		j = 0;
+		for ind in order_centroids[i, :10]:
+			print("\t" + str(terms[ind]))
+			if j < 4:
+				t = t + str(terms[ind]) + str(" ")
+				j = j + 1;
+		cluster_names[i] = t
+		cluster_colors[i] = colors[i]
+		print
+
+	#set up colors per clusters using a dict
+	# cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e'}
+	 	      
+	print(cluster_names)
+	print(cluster_colors)
+
+	# vectorizer = HashingVectorizer(n_features=2**4)
+	# Xs = vectorizer.fit_transform(documents)
+	# print(Xs.shape)
+
+	# tfidf_matrix = tfidf_vectorizer.fit_transform(synopses) #fit the vectorizer to synopses
+	print(X.shape)
+	dist = 1 - cosine_similarity(X)
+
+
+	MDS()
+
+	# convert two components as we're plotting points in a two-dimensional plane
+	# "precomputed" because we provide a distance matrix
+	# we will also specify `random_state` so the plot is reproducible.
+	mds = MDS(n_components=2, dissimilarity="precomputed", random_state=12)
+
+	pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
+
+	xs, ys = pos[:, 0], pos[:, 1]
+
+
+	idx = 0;
+	d = {}
+	for i in range(0, true_k):
+		d[i] = {"x":[], "y":[]}
+
+	for i in model.labels_.tolist():
+		d[model.labels_.tolist()[idx]]["x"].append(xs[idx])
+		d[model.labels_.tolist()[idx]]["y"].append(ys[idx])
+		idx = idx + 1;
+
+	trace = []
+	for i in range(0, true_k):
+		trace.append(go.Scatter(x = d[i]["x"], y = d[i]["y"], name = cluster_names[i],mode = 'markers',marker = dict(size = 10, color = cluster_colors[i], line = dict(width = 2,color = 'rgb(0, 0, 0)'))));
+
+
+	layout = dict(title = 'Styled Scatter', yaxis = dict(zeroline = False), xaxis = dict(zeroline = False))
+
+	fig = dict(data=trace, layout=layout)
+	plotly.offline.plot(fig, filename='kmeans')
+	return
 
 if __name__ == '__main__':
+	# kmeans();
+
 	cats = ["comp.windows.x", "comp.os.ms-windows.misc", "talk.politics.misc", "comp.sys.ibm.pc.hardware","talk.religion.misc","rec.autos","sci.space","talk.politics.guns","alt.atheism","misc.forsale","comp.graphics","sci.electronics","sci.crypt","soc.religion.christian","rec.sport.hockey","sci.med","rec.motorcycles","comp.sys.mac.hardware","talk.politics.mideast","rec.sport.baseball"];
 	catsDescription = ["Alt - Atheism", "Computer - Graphics", "Computer - Microsoft/Windows", "Computer - IBM/PC", "Computer - Mac", "Computer - Windows", "Misc. - For Sale", "Rec. - Autos", "Rec - Motorcycles", "Sports - Baseball", "Sports - Hockey", "Science - Cryptography", "Science - Electronics", "Science - Medicine", "Science - Space", "Religion - Christianity", "Politics - Guns", "Politics - Middle East", "Politics - Misc.", "Religion - Misc."]
 	subcats = ["comp.windows.x", "sci.med", "rec.sport.hockey", "soc.religion.christian"]
@@ -339,9 +471,20 @@ if __name__ == '__main__':
 
 	fullVocab = []
 	lem = WordNetLemmatizer()
+	kmeansDocs = []
+
+	idx = 0
 	for doc in twentyNewsTrain.data: 
+		print(float(idx / len(twentyNewsTrain.data) * 100))
 		docTokens = customPreprocessor(lem, doc)
 		fullVocab.append(docTokens)
+		kmeansDocs.append(" ".join(docTokens))
+		idx = idx + 1;
+
+	# input("doc_tokens: \n" + str(docTokens))
+	
+	# input("kmeans vocab: \n" + str(kmeansDocs))
+	kmeans(kmeansDocs[0:100]);
 
 	docVocabDict = Dictionary(fullVocab)
 
