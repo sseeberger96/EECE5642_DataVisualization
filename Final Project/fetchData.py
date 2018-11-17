47161ea9841701d9f01136c72ff3c9bb910f30a2
sseeberger96 <seeberger.s@husky.neu.edu>
2018-11-17 14:39:33 -0500

    Created Final Project Repository

---
+++ Final Project/fetchData.py
@@ -0,0 +1,26 @@
+import urllib.request
+import json
+import sys
+
+html = urllib.request.urlopen("http://hubwaydatachallenge.org/api/v1/station/?format=json&username=tym0027&api_key=8b29d8b3bdcac800972839130daf4f9b55c0e2ae")
+
+A = html.read().decode('utf-8')
+B = json.loads(A)
+
+''' # Meta data
+print(B["meta"]["limit"])
+print(B["meta"]["next"])
+print(B["meta"]["offset"])
+print(B["meta"]["previous"])
+print(B["meta"]["total_count"])
+'''
+
+csvFile = open("./stationData.txt", 'w')
+csvFile.write("name,id,lat,lon")
+for data in B["objects"]:
+	s = str(data["name"]) +", " + str(data["terminalname"]) + ", " + str(data["point"]["coordinates"][1]) + ", " + str(data["point"]["coordinates"][0])
+	csvFile.write(s + "\n")
+	print(s + "\n")
+
+csvFile.close()
+		
