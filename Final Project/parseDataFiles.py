e991e269e26c630fd34e2c484c0c221f096490b3
sseeberger96 <seeberger.s@husky.neu.edu>
2018-11-20 19:28:56 -0500

    Got trip counter function working

--- Final Project/parseDataFiles.py
+++ Final Project/parseDataFiles.py
@@ -13,6 +13,28 @@ def readData(filename):
 	return data
 
 
+def searchStationIds(data): 
+	stationIds = []
+	for trip in data:
+		if trip['start station id'] not in stationIds: 
+			stationIds.append(trip['start station id']) 
+		if trip['end station id'] not in stationIds: 
+			stationIds.append(trip['end station id'])
+	stationIds = [int(x) for x in stationIds]
+	stationIds.sort()
+	print(stationIds)
+
+def countTrips(data): 
+	numTrips = np.zeros((250,250))
+	for trip in data: 
+		start = int(trip['start station id'])
+		end = int(trip['end station id'])
+		numTrips[start][end] += 1 
+
+	# print(numTrips[200][21])
+
+
+
 			
 
 
@@ -20,6 +42,9 @@ def readData(filename):
 if __name__ == '__main__':
 	monthData = readData("2017_Data/201701-hubway-tripdata.csv")
 
-	print(len(monthData))
-	print(monthData[0])
+	# print(len(monthData))
+	# print(monthData[0])
 	print(monthData[0]['start station id'])
+
+	# searchStationIds(monthData)
+	countTrips(monthData)
